1f9ef4c154dd20d5796fede72ad92ba031c79aff
dkolom <dkolom@gmail.com>
2019-03-04 12:19:28 +0900

    Add comments in the interface header

--- src/core/wrappers.h
+++ src/core/wrappers.h
@@ -30,17 +30,87 @@
 */
 
 /* C/C++ interface */
-/* Encoding subroutine with wavelet transform and range coding */ 
+/* Encoding subroutine with wavelet transform and range coding 
+    nx : (INPUT) number of elements of the input 3D field in the first (fastest) direction
+    ny : (INPUT) number of elements of the input 3D field in the second direction
+    nz : (INPUT) number of elements of the input 3D field in the third (slowest) direction
+    fld_1d : (INPUT) input 3D field reshaped in a 1D array with the direction x being contiguous (as in Fortran)
+    wtflag : (INPUT) wavelet transform flag: 0 if not transforming, 1 if transforming
+    mx : (INPUT) number local cutoff subdomains in x, recommended mx=1
+    my : (INPUT) number local cutoff subdomains in y, recommended my=1
+    mz : (INPUT) number local cutoff subdomains in z, recommended mz=1
+    cutoffvec : (INPUT) local cutoff vector, e.g. cutoffvec = new double[1]; cutoffvec[0] = relative_global_tolerance;
+    tolabs : (OUTPUT) absolute global tolerance
+    midval : (OUTPUT) mid-value, midval = minval+(maxval-minval)/2, where maxval is the maximum and minval is the minimum of fld_1d
+    halfspanval : (OUTPUT) half-span, halfspanval = (maxval-minval)/2
+    wlev : (OUTPUT) number of wavelet transform levels
+    nlay : (OUTPUT) number of bit planes
+    ntot_enc : (OUTPUT) total number of elemens of the encoded array data_enc
+    deps_vec : (OUTPUT) quantization step size vector, double deps_vec[nlaymax]; where nlaymax is an output of setup_wr
+    minval_vec : (OUTPUT) bit plane offset vector, double minval_vec[nlaymax];
+    len_enc_vec : (OUTPUT) number of elements in the encoded bit planes, unsigned long int len_enc_vec[nlaymax];
+    data_enc : (OUTPUT) range-encoded output data array, defined as, e.g., unsigned char *data_enc = new unsigned char[ntot_enc_max]; where ntot_enc_max is an output of setup_wr */ 
 extern "C" void encoding_wrap(int nx, int ny, int nz, double *fld_1d, int wtflag, int mx, int my, int mz, double *cutoffvec, double& tolabs, double& midval, double& halfspanval, unsigned char& wlev, unsigned char& nlay, unsigned long int& ntot_enc, double *deps_vec, double *minval_vec, unsigned long int *len_enc_vec, unsigned char *data_enc);
-/* Decoding subroutine with range decoding and inverse wavelet transform*/ 
+
+/* Decoding subroutine with range decoding and inverse wavelet transform 
+    nx : (INPUT) number of elements of the input 3D field in the first (fastest) direction
+    ny : (INPUT) number of elements of the input 3D field in the second direction
+    nz : (INPUT) number of elements of the input 3D field in the third (slowest) direction
+    fld_1d : (OUTPUT) output reconstructed 3D field reshaped in a 1D array with the direction x being contiguous (as in Fortran)
+    tolabs : (NOT USED)
+    midval : (INPUT) mid-value, midval = minval+(maxval-minval)/2, where maxval is the maximum and minval is the minimum of fld_1d
+    halfspanval : (INPUT) half-span, halfspanval = (maxval-minval)/2
+    wlev : (INPUT) number of wavelet transform levels
+    nlay : (INPUT) number of bit planes
+    ntot_enc : (INPUT) total number of elemens of the encoded array data_enc
+    deps_vec : (INPUT) quantization step size vector, double deps_vec[nlay];
+    minval_vec : (INPUT) bit plane offset vector, double minval_vec[nlay];
+    len_enc_vec : (INPUT) number of elements in the encoded bit planes, unsigned long int len_enc_vec[nlay];
+    data_enc : (INPUT) range-encoded data array, defined as, e.g., unsigned char *data_enc = new unsigned char[ntot_enc]; */ 
 extern "C" void decoding_wrap(int nx, int ny, int nz, double *fld_1d, double& tolabs, double& midval, double& halfspanval, unsigned char& wlev, unsigned char& nlay, unsigned long int& ntot_enc, double *deps_vec, double *minval_vec, unsigned long int *len_enc_vec, unsigned char *data_enc);
-/* Return the number of bit planes and the required encoded data array size */ 
+
+/* Return the number of bit planes and the required encoded data array size, as needed for memory allocation
+    nlaymax : maximum allowed number of bit planes
+    ntot_enc_max : maximum allowed total number of elemens of the encoded array data_enc */ 
 extern "C" void setup_wr(int nx, int ny, int nz, unsigned char& nlaymax, unsigned long int& ntot_enc_max);
 
 /* Fortran interface */
-/* Encoding subroutine with wavelet transform and range coding */ 
+/* Encoding subroutine with wavelet transform and range coding 
+    nx : (INPUT) number of elements of the input 3D field in the first (fastest) direction
+    ny : (INPUT) number of elements of the input 3D field in the second direction
+    nz : (INPUT) number of elements of the input 3D field in the third (slowest) direction
+    fld : (INPUT) output reconstructed 3D field reshaped in a 1D array with the direction x being contiguous (as in Fortran)
+    wtflag : (INPUT) wavelet transform flag: 0 if not transforming, 1 if transforming
+    tolrel : (INPUT) relative global tolerance
+    tolabs : (OUTPUT) absolute global tolerance
+    midval : (OUTPUT) mid-value, midval = minval+(maxval-minval)/2, where maxval is the maximum and minval is the minimum of fld
+    halfspanval : (OUTPUT) half-span, halfspanval = (maxval-minval)/2
+    wlev : (OUTPUT) number of wavelet transform levels
+    nlay : (OUTPUT) number of bit planes
+    ntot_enc : (OUTPUT) total number of elemens of the encoded array data_enc
+    deps_vec : (OUTPUT) quantization step size vector
+    minval_vec : (OUTPUT) bit plane offset vector
+    len_enc_vec : (OUTPUT) number of elements in the encoded bit planes
+    data_enc : (OUTPUT) range-encoded output data array */ 
 extern "C" void encoding_wrap_f(int *nx, int *ny, int *nz, double *fld, int *wtflag, double *tolrel, double& tolabs, double& midval, double& halfspanval, unsigned char& wlev, unsigned char& nlay, long int& ntot_enc, double *deps_vec, double *minval_vec, long int *len_enc_vec, unsigned char *data_enc);
-/* Decoding subroutine with range decoding and inverse wavelet transform*/ 
+
+/* Decoding subroutine with range decoding and inverse wavelet transform 
+    nx : (INPUT) number of elements of the input 3D field in the first (fastest) direction
+    ny : (INPUT) number of elements of the input 3D field in the second direction
+    nz : (INPUT) number of elements of the input 3D field in the third (slowest) direction
+    fld : (OUTPUT) output reconstructed 3D field reshaped in a 1D array with the direction x being contiguous (as in Fortran)
+    midval : (INPUT) mid-value, midval = minval+(maxval-minval)/2, where maxval is the maximum and minval is the minimum of fld
+    halfspanval : (INPUT) half-span, halfspanval = (maxval-minval)/2
+    wlev : (INPUT) number of wavelet transform levels
+    nlay : (INPUT) number of bit planes
+    ntot_enc : (INPUT) total number of elemens of the encoded array data_enc
+    deps_vec : (INPUT) quantization step size vector
+    minval_vec : (INPUT) bit plane offset vector
+    len_enc_vec : (INPUT) number of elements in the encoded bit planes
+    data_enc : (INPUT) range-encoded data array */ 
 extern "C" void decoding_wrap_f(int *nx, int *ny, int *nz, double *fld, double& midval, double& halfspanval, unsigned char& wlev, unsigned char& nlay, long int& ntot_enc, double *deps_vec, double *minval_vec, long int *len_enc_vec, unsigned char *data_enc);
-/* Return the number of bit planes and the required encoded data array size */ 
+
+/* Return the number of bit planes and the required encoded data array size, as needed for memory allocation
+    nlaymax : maximum allowed number of bit planes
+    ntot_enc_max : maximum allowed total number of elemens of the encoded array data_enc */ 
 extern "C" void setup_wr_f(int *nx, int *ny, int *nz, int& nlaymax, long int& ntot_enc_max);
