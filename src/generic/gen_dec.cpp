996f87e28d5f838eacd5450571d864ec31b8c3a9
Dmitry Kolomenskiy <dkolom@gmail.com>
2019-06-26 22:49:20 -0700

    Update gen_dec.cpp

--- src/generic/gen_dec.cpp
+++ src/generic/gen_dec.cpp
@@ -67,7 +67,7 @@ int main( int argc, char *argv[] )
     int nh;
     int nzh;
 
-    // Base tolerance, applied as relative to max(abs(fld_1d))
+    // Base tolerance, applied as relative to max(fabs(fld_1d))
     double tol_base;
 
     // Floating point input file endian conversion flag (0: do not convert; 1: convert)
